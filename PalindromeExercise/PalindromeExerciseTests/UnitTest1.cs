c9d04c709d52509dc735e6324feda3d81315e33d
Miles Mao <mrmao1@gmail.com>
2023-01-27 13:02:07 -0600

    Palindrome exercise results

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,27 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("palindrome", false)]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        [InlineData ("kayak",true)]
+        [InlineData("madam", true)]
+        [InlineData("Kayak", true)]
+        [InlineData("kaYak", true)]
+        [InlineData("Jeremy", false)]
+        [InlineData("tenet", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            var tester = new WordSmith();
 
+            //Act
+            var actual = tester.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
